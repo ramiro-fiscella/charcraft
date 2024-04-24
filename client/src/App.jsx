37bb70cea758b003b89a5f41f41514c244d9a532
Ramirof <ramirofiscella@gmail.com>
2024-04-24 19:53:15 -0300

    made Card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import Card from "./components/Card.jsx/Card";
+import CharactersView from "./views/CharactersView/CharactersView";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello world!</h1>
-      <Card />
+      <CharactersView />
     </>
   );
 }
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,13 +4,13 @@ const Card = () => {
   return (
     // CARD CONTAINER
     <div
-      className="w-[224px] h-[308px] border-2 rounded-lg bg-neutral-900"
+      className="w-[224px] h-[308px] border-2 border-neutral-900 rounded-lg bg-neutral-900"
       id="card"
     >
       <div className="m-2 h-[90%] pt-2 px-1 mb-2 flex-row rounded rounded-b-2xl bg-neutral-200">
         {/* NOMBRE Y NIVEL */}
         <div
-          className="w-[100%] h-6 p-[2px] flex justify-between content-center border-[1px] border-neutral-900 rounded-sm text-neutral-900"
+          className="w-[100%] h-6 p-[2px] flex justify-between content-center border-[1px] border-neutral-600 rounded-sm text-neutral-900"
           aria-label="Name and level of the character"
         >
           <h1 className="text-sm">Nombre</h1>
@@ -20,15 +20,15 @@ const Card = () => {
         {/* IMAGE */}
         <div className="mx-1 h-36">
           <img
-            className="w-full h-full object-cover outline-double outline-neutral-900"
-            src="https://static01.nyt.com/images/2014/11/03/arts/GARDEN/GARDEN-superJumbo.jpg"
+            className="w-full h-full object-cover object-top outline-double outline-neutral-600"
+            src="assets/character.jpg"
             alt=" Character's image"
           />
         </div>
 
         {/* RACE-CLASS */}
         <div
-          className="w-[100%] h-6 p-[2px] flex justify-between items-end border-[1px] border-neutral-900 rounded-sm text-neutral-900"
+          className="w-[100%] h-6 p-[2px] flex justify-between items-end border-[1px] border-neutral-600 rounded-sm text-neutral-900"
           aria-label="Race and class of the character"
         >
           <h4 className="text-xs">Race - class</h4>
@@ -38,14 +38,12 @@ const Card = () => {
         {/* INFO */}
         <div className="mx-1 h-36">
           <div
-            className="w-full h-[80px] min-h-[80px] p-2 outline outline-1 outline-neutral-900 rounded-b bg-neutral-200 overflow-hidden hover:overflow-visible hover:h-auto"
+            className="w-full h-[80px] min-h-[80px] p-2 outline outline-1 outline-neutral-400 rounded-b bg-neutral-200 overflow-hidden hover:overflow-visible hover:h-auto"
             aria-label="Character's quote"
           >
-            <p className="text-xs overflow-wrap break-word">
-              Lorem i met consectetur adipisicing elit. Obcaecati ipsa corrupti
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-              culpa non doloribus? Cumque beatae nobis assumenda numquam ad,
-              aspernatur laboriosam?
+            <p className="text-xs leading-4 font-medium text-neutral-800 overflow-wrap break-word">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+              impedit perspiciatis praesentium obcaecati, maiores iste.
             </p>
           </div>
         </div>
@@ -54,7 +52,7 @@ const Card = () => {
       <div className="mx-2 mt-3 flex justify-between items-end ">
         <p className="text-[6px] text-neutral-300 text-left">RKF TTCD ©</p>
         <p className="text-[6px] text-neutral-300 text-left">
-          Some rights reserved.
+          Charcraft · 2024
         </p>
       </div>
     </div>
