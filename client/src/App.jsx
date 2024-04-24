8c9a1c19b7ba53166802575f2f2ba6bbedb5cb30
Ramirof <ramirofiscella@gmail.com>
2024-04-24 15:45:31 -0300

    tailwind init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
       <Card />
     </>
   );
--- client/src/components/Card.jsx/Card.jsx
+++ client/src/components/Card.jsx/Card.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Card = () => {
-  return;
-
-  <></>;
+  return (
+    <>
+      <h1 className="">CARD</h1>
+    </>
+  );
 };
 
 export default Card;
