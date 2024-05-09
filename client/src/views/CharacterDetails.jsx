762fb687b252581723e7852da053c827a165a332
Ramirof <ramirofiscella@gmail.com>
2024-05-09 11:40:57 -0300

    skills

--- client/src/views/CharacterDetails.jsx
+++ client/src/views/CharacterDetails.jsx
@@ -30,119 +30,194 @@ const CharacterDetails = () => {
 
   return (
     <div className="h-full w-full flex flex-wrap m-auto mt-14 p-5">
-      <div className="max-w-[1200px] m-auto flex flex-col gap-4">
-        <div id="character" className="data-container">
-          <h1 className="text-3xl">{character.char_name}</h1>
-          <img
-            className="w-[200px] h-[200px] object-cover object-center rounded-md"
-            src={character.avatar_url}
-            alt={`${character.char_name} avatar`}
-          />
+      <div className="max-w-[1200px] flex">
+        <div className="w-full flex flex-col gap-4">
+          <div className="w-full h-56 flex flex-row items-start gap-4">
+            <img
+              className="w-[200px] h-[200px] object-cover object-center rounded-md"
+              src={character.avatar_url}
+              alt={`${character.char_name} avatar`}
+            />
 
-          <div id="class" className="">
-            <p>{character.race}</p>
-            <p> {character.char_class}</p>
+            <div id="class" className="flex flex-col">
+              <h1 className="text-3xl mb-2">{character.char_name}</h1>
+              <p>
+                {character.race} - {character.char_class}
+              </p>
+              <p>Level {character.level}</p>
+            </div>
           </div>
-          <p>Level: {character.level}</p>
+          <div className="flex flex-row gap-4">
+            <div id="attributes">
+              <ul>
+                <li>
+                  <h5>Strenght</h5>
+                  <p>{character.strength}</p>
+                  <h6>+2</h6>
+                </li>
+
+                <li>
+                  <h5>Dexterity</h5>
+                  <p> {character.dexterity}</p>
+                  <h6>+2</h6>
+                </li>
+
+                <li>
+                  <h5>Constitution</h5>
+                  <p>{character.constitution}</p>
+                  <h6>+2</h6>
+                </li>
+
+                <li>
+                  <h5>Intelligence</h5>
+                  <p>{character.intelligence}</p>
+                  <h6>+2</h6>
+                </li>
+
+                <li>
+                  <h5>Wisdom</h5>
+                  <p>{character.wisdom}</p>
+                  <h6>+2</h6>
+                </li>
+
+                <li>
+                  <h5>Charisma</h5>
+                  <p>{character.charisma}</p>
+                  <h6>+2</h6>
+                </li>
+              </ul>
+            </div>
+
+            <div id="skills" className="w-58 border-2 p-4 rounded-lg">
+              <ul className="flex flex-col items-start gap-2">
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Acrobatics: {character.acrobatics}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Animal Handling: {character.animal_handling}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Arcana: {character.arcana}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Athletics: {character.athletics}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Deception: {character.deception}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>History: {character.history}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Insight: {character.insight}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Intimidation: {character.intimidation}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Investigation: {character.investigation}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Medicine: {character.medicine}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Nature: {character.nature}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Perception: {character.perception}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Performance: {character.performance}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Persuasion: {character.persuasion}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Religion: {character.religion}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Sleight of Hand: {character.sleight_of_hand}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Stealth: {character.stealth}</p>
+                </li>
+                <li>
+                  <input type="radio" />
+                  <span>+2</span>
+                  <p>Survival: {character.survival}</p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
 
-        <div id="attributes">
-          <ul>
-            <li>
-              <h5>Strenght</h5>
-              <p>{character.strength}</p>
-              <h6>+2</h6>
-            </li>
-
-            <li>
-              <h5>Dexterity</h5>
-              <p> {character.dexterity}</p>
-              <h6>+2</h6>
-            </li>
-
-            <li>
-              <h5>Constitution</h5>
-              <p>{character.constitution}</p>
-              <h6>+2</h6>
-            </li>
-
-            <li>
-              <h5>Intelligence</h5>
-              <p>{character.intelligence}</p>
-              <h6>+2</h6>
-            </li>
-
-            <li>
-              <h5>Wisdom</h5>
-              <p>{character.wisdom}</p>
-              <h6>+2</h6>
-            </li>
-
-            <li>
-              <h5>Charisma</h5>
-              <p>{character.charisma}</p>
-              <h6>+2</h6>
-            </li>
-          </ul>
+        <div id="personality" className="data-container">
+          <h2 className="text-2xl">Personality</h2>
+          <p>Aligment: {character.aligment}</p>
+          <p>Personality: {character.personality_traits}</p>
+          <p>Ideals: {character.ideals}</p>
+          <p>Bonds: {character.bonds}</p>
+          <p>Flaws: {character.flaws}</p>
+          <p>Quote: {character.quote}</p>
+          <p>Features_adn_traits: {character.features_adn_traits}</p>
+          <p>Languajes: {character.languajes}</p>
+          <p>Other_proficiencies: {character.other_proficiencies}</p>
         </div>
-      </div>
-
-      <div id="skills" className="data-container">
-        <h2 className="text-2xl">Skills</h2>
-        <p>Acrobatics: {character.acrobatics}</p>
-        <p>Animal Handling: {character.animal_handling}</p>
-        <p>Arcana: {character.arcana}</p>
-        <p>Athletics: {character.athletics}</p>
-        <p>Deception: {character.deception}</p>
-        <p>History: {character.history}</p>
-        <p>Insight: {character.insight}</p>
-        <p>Intimidation: {character.intimidation}</p>
-        <p>Investigation: {character.investigation}</p>
-        <p>Medicine: {character.medicine}</p>
-        <p>Nature: {character.nature}</p>
-        <p>Perception: {character.perception}</p>
-        <p>Performance: {character.performance}</p>
-        <p>Persuasion: {character.persuasion}</p>
-        <p>Religion: {character.religion}</p>
-        <p>Sleight of Hand: {character.sleight_of_hand}</p>
-        <p>Stealth: {character.stealth}</p>
-        <p>Survival: {character.survival}</p>
-      </div>
-
-      <div id="personality" className="data-container">
-        <h2 className="text-2xl">Personality</h2>
-        <p>Aligment: {character.aligment}</p>
-        <p>Personality: {character.personality_traits}</p>
-        <p>Ideals: {character.ideals}</p>
-        <p>Bonds: {character.bonds}</p>
-        <p>Flaws: {character.flaws}</p>
-        <p>Quote: {character.quote}</p>
-        <p>Features_adn_traits: {character.features_adn_traits}</p>
-        <p>Languajes: {character.languajes}</p>
-        <p>Other_proficiencies: {character.other_proficiencies}</p>
-      </div>
-
-      <div id="combat_stats" className="data-container">
-        <h2 className="text-2xl">Combat Stats</h2>
-        <p>Max HP: {character.max_hp}</p>
-        <p>Current HP: {character.current_hp}</p>
-        <p>Temp HP: {character.temp_hp}</p>
-        <p>Armor Class: {character.armor_class}</p>
-        <p>Initiative: {character.initiative}</p>
-        <p>Speed: {character.speed}</p>
-        <p>Hit Dice: {character.hit_dice}</p>
-        <p>Total Hit Dice: {character.total_hit_dice}</p>
-        <p>Death Save Success: {character.death_save_success}</p>
-        <p>Death Save Failure: {character.death_save_failure}</p>
-      </div>
-
-      <div id="attack_stats" className="data-container">
-        <h2 className="text-2xl">Attack Stats</h2>
-        <p>Weapon: {character.weapon}</p>
-        <p>Attack Bonus: {character.atk_bonus}</p>
-        <p>Damage: {character.damage}</p>
-        <p>Damage Type: {character.damage_type}</p>
-        <p>Attack Range: {character.atk_range}</p>
+        <div id="combat_stats" className="data-container">
+          <h2 className="text-2xl">Combat Stats</h2>
+          <p>Max HP: {character.max_hp}</p>
+          <p>Current HP: {character.current_hp}</p>
+          <p>Temp HP: {character.temp_hp}</p>
+          <p>Armor Class: {character.armor_class}</p>
+          <p>Initiative: {character.initiative}</p>
+          <p>Speed: {character.speed}</p>
+          <p>Hit Dice: {character.hit_dice}</p>
+          <p>Total Hit Dice: {character.total_hit_dice}</p>
+          <p>Death Save Success: {character.death_save_success}</p>
+          <p>Death Save Failure: {character.death_save_failure}</p>
+        </div>
+        <div id="attack_stats" className="data-container">
+          <h2 className="text-2xl">Attack Stats</h2>
+          <p>Weapon: {character.weapon}</p>
+          <p>Attack Bonus: {character.atk_bonus}</p>
+          <p>Damage: {character.damage}</p>
+          <p>Damage Type: {character.damage_type}</p>
+          <p>Attack Range: {character.atk_range}</p>
+        </div>
       </div>
     </div>
   );
