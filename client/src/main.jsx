5b6bcfb0bbc4f3625c9e6c1edccea6201d21bbec
Ramirof <ramirofiscella@gmail.com>
2024-06-07 17:06:51 -0300

    auth0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,9 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-m84y6use4udzz5ud.us.auth0.com"
-      clientId="oVG9ftoMcxBvJ88W1qezhaoGwUZt8atY"
+      clientId="Ipij3vAyO4HkNpFTvlhObpKG31kGT40z"
       authorizationParams={{
         redirect_uri: window.location.origin,
+        audience: 'https://dev-m84y6use4udzz5ud.us.auth0.com/api/v2/',
       }}
     >
       <BrowserRouter>
