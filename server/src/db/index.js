8c21819de2f9bb51e6c7914560c72181f92fae1b
Ramirof <ramirofiscella@gmail.com>
2024-06-08 19:13:31 -0300

    updated env

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,11 +1,5 @@
-// require("dotenv").config();
+require('dotenv').config();
 const { Pool } = require('pg');
-
-// const POSTGRES_DATABASEBASE = process.env.POSTGRES_DATABASEBASE;
-// const POSTGRES_HOST = process.env.POSTGRES_HOST;
-// const POSTGRES_PASSWORDORD = process.env.POSTGRES_PASSWORDORD;
-// const PGPORT = process.env.PGPORT;
-// const PPOSTGRES_USER = process.env.PPOSTGRES_USER;
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
