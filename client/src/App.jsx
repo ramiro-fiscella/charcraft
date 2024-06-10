2bed0ae904ebd08fd302581a150305f9cdb1cfb1
Ramirof <ramirofiscella@gmail.com>
2024-06-10 16:17:26 -0300

    updated dotenv

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,9 @@ import { NavBar, Footer } from './components';
 
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/';
-// 'postgres://default:h0umCvMfE8aF@ep-broad-bread-a4i687er.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require';
+const api_url = import.meta.env.VITE_API_URL;
+
+axios.defaults.baseURL = api_url;
 
 import './App.css';
 
