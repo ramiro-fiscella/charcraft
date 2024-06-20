346937d41aee13b9ddb9031c18d961791d460261
kafz <ramirofiscella@gmail.com>
2024-06-20 12:47:15 -0300

    updated deps

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const { Client } = require('pg');
 const router = require('./src/routes');
 
 const app = express();
@@ -10,13 +11,25 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(helmet());
-
 app.use(express.json());
 app.use(morgan('dev'));
 
 // routes
 app.use('/', router);
 
+// Connect to PostgreSQL
+const client = new Client({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client
+  .connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch((err) => console.error('Database connection error:', err.stack));
+
 // start server
-const PORT = process.env.POSTGRES_URL || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
